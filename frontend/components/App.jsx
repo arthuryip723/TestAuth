3a68422d4b1f6ee18d686b01f42c6799b600c5b2
Arthur Yip <arthuryip723@gmail.com>
2016-10-18 18:48:35 -0400

    playing with it

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 
-const App = ({children}) => (
-  <div>
-    <header>
-      <h1>
-        This is Header!
-      </h1>
-    </header>
-    {children}
-  </div>
-);
+// const App = ({children}) => (
+//   <div>
+//     <header>
+//       <h1>
+//         This is Header!
+//       </h1>
+//     </header>
+//     {children}
+//   </div>
+// );
+
+const App = React.createClass({
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>
+            This is Header!
+          </h1>
+        </header>
+        {this.props.children}
+      </div>
+    );
+  }
+});
 
 export default App;
